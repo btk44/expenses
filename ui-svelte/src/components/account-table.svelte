2504a12ac7757d38af381697f7a67289cfdc3bad
bk <btko@tuta.io>
2024-05-20 11:57:33 +0200

    account table refreshing on dbclick

--- ui-svelte/src/components/account-table.svelte
+++ ui-svelte/src/components/account-table.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-    import { accountStoreReadOnly, accountCurrencyMap } from '../services/store'
+    import { accountStoreReadOnly, accountCurrencyMap, reloadAccount } from '../services/store'
   
     const headers = ['id', 'nazwa', 'kwota', '-'] 
     const accountCurrency = accountCurrencyMap()
 
+    let selectedRow = -1
     let accounts = $accountStoreReadOnly
     accountStoreReadOnly.subscribe((data) => {accounts = data})
 
     const formatAmount = (amount: number): string => { 
         return amount < 0 ? amount.toString() : `${amount}` 
+    }
+
+    const recalculateAccountAmounts = async (accountId: number) => { 
+        try { await reloadAccount(1, accountId) }
+        catch { alert('server error') }
+        finally { selectedRow = -1 }
     }
 </script>
 
@@ -19,9 +26,9 @@
             <th class="aln-l">{header}</th>
             {/each}
         </tr>
-        {#each Object.entries(accounts) as [accountId, account]}
+        {#each Object.entries(accounts) as [accountId, account], index}
         {#if account.active}
-        <tr>
+        <tr class={selectedRow === index ? 'selected' : ''} on:dblclick={() => { selectedRow = index; recalculateAccountAmounts(+accountId) }}>
             <td class="aln-c w-10pc">{accountId}</td>
             <td class="aln-l w-50pc">{account.name}</td>
             <td class="aln-r w-30pc">{formatAmount(account.amount)}</td>
@@ -34,6 +41,7 @@
 
 <style lang="scss">
     @import '../styles/app.scss';
-    
+
     .data{ overflow-y: auto; height: 100%;} 
+    .selected { background-color: $accent-color-light;}
 </style>
