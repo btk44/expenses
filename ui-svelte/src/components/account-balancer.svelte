603d60fead1a67cf7e16060633c2c386d388b7e9
bk <btko@tuta.io>
2024-05-24 13:02:40 +0200

    account balance helper started

---
+++ ui-svelte/src/components/account-balancer.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { type Account } from "../models/account";
+
+    export let account: Account
+    let realAmount = 0
+    let cash = 0
+    $: missingAmount = Math.round((+account.amount - (+realAmount + +cash)) *100) / 100
+</script>
+
+<div class="account-balancer">
+    <div class="control-group">
+        <label for="calculatedAmount">stan w aplikacji:</label>
+        <input type="text" name="calculatedAmount" readonly bind:value={account.amount}/>
+    </div>
+    <div class="control-group">
+        <label for="realAmount">fizycznie na koncie:</label>
+        <input type="text" name="realAmount" bind:value={realAmount}/>
+    </div>
+    <div class="control-group">
+        <label for="cashAmount">gotówka:</label>
+        <input type="text" name="cashAmount" bind:value={cash}/>
+    </div>
+    <div class="control-group">
+        <label for="missingAmount">manko:</label>
+        <input type="text" name="missingAmount" readonly bind:value={missingAmount}/>
+    </div>
+    <div>
+        <input type="text" />
+        <button class="button-outlined">OK</button>
+    </div>
+</div>
+
+<style lang="scss">
+    @import '../styles/app.scss';
+
+    .account-balancer { 
+        display: flex;  flex-direction: column; gap: 10px; justify-content: space-between;
+        border: 3px solid $primary-color;
+        padding: 10px;
+        .control-group { 
+            display: flex; justify-content: start; flex-direction: column;
+            label { margin: 0 5px; }
+        }
+    }
+</style>
--- ui-svelte/src/components/account-table.svelte
+++ ui-svelte/src/components/account-table.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { type Account } from '../models/account';
     import { accountStoreReadOnly, accountCurrencyMap, reloadAccount } from '../services/store'
+	import AccountBalancer from './account-balancer.svelte';
   
     const headers = ['id', 'nazwa', 'kwota', '-'] 
     const accountCurrency = accountCurrencyMap()
@@ -17,31 +19,53 @@
         catch { alert('server error') }
         finally { selectedRow = -1 }
     }
+
+
+    let accountToBalance: Account = null
+    const openAccountBalancer = (account: Account) => {
+        accountToBalance = account
+    }
 </script>
 
-<div class="data">
-    <table>
-        <tr>
-            {#each headers as header}
-            <th class="aln-l">{header}</th>
+<div class="accounts">
+    {#if accountToBalance == null}
+    <div class="data">
+        <table>
+            <tr>
+                {#each headers as header}
+                <th class="aln-l">{header}</th>
+                {/each}
+            </tr>
+            {#each Object.entries(accounts) as [accountId, account], index}
+            {#if account.active}
+            <!-- <tr class={selectedRow === index ? 'selected' : ''} on:dblclick={() => { selectedRow = index; recalculateAccountAmounts(+accountId) }}> -->
+            <tr class={selectedRow === index ? 'selected' : ''} on:dblclick={() => { openAccountBalancer(account) }}>
+                <td class="aln-c w-10pc">{accountId}</td>
+                <td class="aln-l w-50pc">{account.name}</td>
+                <td class="aln-r w-30pc">{formatAmount(account.amount)}</td>
+                <td class="aln-l w-10pc">{accountCurrency[accountId] ?? ''}</td>
+            </tr>
+            {/if}
             {/each}
-        </tr>
-        {#each Object.entries(accounts) as [accountId, account], index}
-        {#if account.active}
-        <tr class={selectedRow === index ? 'selected' : ''} on:dblclick={() => { selectedRow = index; recalculateAccountAmounts(+accountId) }}>
-            <td class="aln-c w-10pc">{accountId}</td>
-            <td class="aln-l w-50pc">{account.name}</td>
-            <td class="aln-r w-30pc">{formatAmount(account.amount)}</td>
-            <td class="aln-l w-10pc">{accountCurrency[accountId] ?? ''}</td>
-        </tr>
-        {/if}
-        {/each}
-    </table>
+        </table>
+    </div>
+    {/if}
+    {#if accountToBalance != null}
+    <div class="account-balancer">
+        <AccountBalancer account={accountToBalance}></AccountBalancer>
+    </div>
+    {/if}
 </div>
 
 <style lang="scss">
     @import '../styles/app.scss';
 
-    .data{ overflow-y: auto; height: 100%;} 
-    .selected { background-color: $accent-color-light;}
+    .accounts{
+        .data{ 
+            overflow-y: auto; height: 100%;
+            .selected { background-color: $accent-color-light;}        
+        }
+        .account-balancer { width: 100%; height: 100%; background-color: white; }
+    }
+
 </style>
